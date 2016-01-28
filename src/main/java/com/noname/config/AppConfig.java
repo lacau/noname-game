9c736f14ad32697bb10a4810c94847cbc7931f75
Lacau <maz_llacau@uolinc.com>
2016-01-28 20:43:52 -0200

    Add String/Hibernate coniguration.

---
+++ src/main/java/com/noname/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.noname.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by lacau on 28/01/16.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.noname")
+public class AppConfig {
+}

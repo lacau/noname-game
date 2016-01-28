547cfa951d52c27e4acee95c7ad371080a64b26b
Lacau <maz_llacau@uolinc.com>
2016-01-28 21:53:44 -0200

    Add @Component to EndPoints.

--- src/main/java/com/noname/endpoint/HistoryEndPoint.java
+++ src/main/java/com/noname/endpoint/HistoryEndPoint.java
@@ -1,10 +1,13 @@
 package com.noname.endpoint;
 
+import org.springframework.stereotype.Component;
+
 import javax.ws.rs.Path;
 
 /**
  * Created by lacau on 28/01/16.
  */
+@Component
 @Path("/history")
 public class HistoryEndPoint {
 }

b700f3a1d84403f78aedbf071a793d63357749dc
Lacau <maz_llacau@uolinc.com>
2016-01-28 17:34:30 -0200

    EndPoints initial definition.

---
+++ src/main/java/com/noname/endpoint/HeroEndPoint.java
@@ -0,0 +1,10 @@
+package com.noname.endpoint;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by lacau on 28/01/16.
+ */
+@Path("/hero")
+public class HeroEndPoint {
+}
---
+++ src/main/java/com/noname/endpoint/PlayerEndPoint.java
@@ -0,0 +1,10 @@
+package com.noname.endpoint;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by lacau on 28/01/16.
+ */
+@Path("/player")
+public class PlayerEndPoint {
+}

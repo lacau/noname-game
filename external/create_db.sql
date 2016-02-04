ece9b18cd6b1eaf610067d30e7c92af7c23984de
Lacau <maz_llacau@uolinc.com>
2016-02-04 18:24:45 -0200

    Change serial8 to serial.

--- external/create_db.sql
+++ external/create_db.sql
@@ -1,34 +1,34 @@
 CREATE TABLE HERO (
-    cd_id serial8 PRIMARY KEY NOT NULL,
+    cd_id serial PRIMARY KEY NOT NULL,
     st_name VARCHAR(30) NOT NULL,
     nr_level INTEGER NOT NULL
 );
 
 CREATE TABLE SKILL (
-    cd_id serial8 PRIMARY KEY NOT NULL,
+    cd_id serial PRIMARY KEY NOT NULL,
     st_name VARCHAR(30) NOT NULL,
     st_description VARCHAR(200) NOT NULL,
     nr_power INTEGER NOT NULL
 );
 
 CREATE TABLE ITEM (
-    cd_id serial8 PRIMARY KEY NOT NULL,
+    cd_id serial PRIMARY KEY NOT NULL,
     st_name VARCHAR(30) NOT NULL,
     st_description VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE ACHIEVEMENT (
-    cd_id serial8 PRIMARY KEY NOT NULL,
+    cd_id serial PRIMARY KEY NOT NULL,
     st_name VARCHAR(30) NOT NULL,
     st_description VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE ARENA (
-    cd_id serial8 PRIMARY KEY NOT NULL,
+    cd_id serial PRIMARY KEY NOT NULL,
     st_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE NPC (
-    cd_id serial8 PRIMARY KEY NOT NULL,
+    cd_id serial PRIMARY KEY NOT NULL,
     st_name VARCHAR(30) NOT NULL
 );

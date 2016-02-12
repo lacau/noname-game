bfe7521d8f403a019c1aa33b123e5d527d98e91d
Lacau <maz_llacau@uolinc.com>
2016-02-12 19:11:06 -0200

    Add LoginEndPoint methods. Add ehCache.xml file.

--- src/main/java/com/noname/server/endpoint/security/LoginEndPoint.java
+++ src/main/java/com/noname/server/endpoint/security/LoginEndPoint.java
@@ -1,7 +1,12 @@
 package com.noname.server.endpoint.security;
 
+import javax.validation.Valid;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
+import com.noname.server.json.CredentialIn;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,4 +15,18 @@ import org.springframework.stereotype.Component;
 @Component
 @Path("/login")
 public class LoginEndPoint {
+
+    @POST
+    @Path("/token")
+    public Response byToken(@HeaderParam("auth_token") String authToken) {
+        if(authToken == null)
+            return Response.status(417).build();
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("/credential")
+    public Response byCredential(@Valid CredentialIn credentialIn) {
+        return Response.ok().build();
+    }
 }

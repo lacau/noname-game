2c360f17dc8a458a6e52a7ab734a135363fcf471
Lacau <maz_llacau@uolinc.com>
2016-02-12 15:59:34 -0200

    Add AccountService.

---
+++ src/main/java/com/noname/server/service/AccountService.java
@@ -0,0 +1,15 @@
+package com.noname.server.service;
+
+import com.noname.server.json.CredentialIn;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by lacau on 12/02/16.
+ */
+@Service
+public class AccountService {
+
+    public void createAccount(CredentialIn credentialIn) {
+
+    }
+}

18b86906c6766f56d675795e39edf86830bdbcc0
Lacau <maz_llacau@uolinc.com>
2016-02-18 21:11:10 -0200

    Add attributes to SkillOut class.

--- src/main/java/com/noname/server/json/SkillOut.java
+++ src/main/java/com/noname/server/json/SkillOut.java
@@ -11,6 +11,10 @@ public class SkillOut implements Serializable {
 
     private String name;
 
+    private String description;
+
+    private Integer power;
+
     private Integer level;
 
     public String getName() {
@@ -19,6 +23,22 @@ public class SkillOut implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
     }
 
     public Integer getLevel() {
